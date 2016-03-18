85ff62d22eedc7ba668d493d14c30cfdd2033db6
Jack Stanley <jackstanley1000@gmail.com>
2016-03-18 12:41:00 -0700

    march18

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
-
   $("#menu-button").click(function(){
     $(this).toggleClass("typcn-times").toggleClass("typcn-th-menu");
     $("#menu").slideToggle("medium");
+  });
+
+  $("#report").click(function(){
+    $("#report-menu").slideToggle("medium");
   });
 });
